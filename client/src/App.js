a2375a27ff8fef3c81ca7482e80c1ceda6f161b0
Om Kokate <om.kokate2902@gmail.com>
2025-03-21 11:45:14 +0530

    react auth

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GoogleLogin from "./components/GoogleLogin";
+
+function App() {
+  return (
+    <div>
+      <h1>Google Authentication</h1>
+      <GoogleLogin />
+    </div>
+  );
+}
+
+export default App;
